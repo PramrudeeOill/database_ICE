4d1c4457cc0512aeffde268258aa1389f8a8c8bf
PramrudeeOill <Pramrudee0141@gmail.com>
2016-04-26 11:35:09 +0700

    finish // clickSingUP blackground

--- app/src/main/java/ice_pbru/pramrudeevajasen/ice_database/MainActivity.java
+++ app/src/main/java/ice_pbru/pramrudeevajasen/ice_database/MainActivity.java
@@ -1,13 +1,41 @@
 package ice_pbru.pramrudeevajasen.ice_database;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+        //bind widget
+
+        bindwidget();
+
+    }//Main Method
+
+    public void clickSignUp (View view) {
+        startActivity(new Intent(MainActivity.this, SignUp.class));
+
+
+    }//clickSingUp
+
+
+    private void bindwidget() {
+
+
+
+    }//bind widget
+
+
+}//Main Class
+
+
